a5ced5efbcb03e2d7ed5498be94950efb44a61e1
bmk10 <gchipunov@hotmail.com>
2016-03-25 15:15:20 -0700

    part 3

--- mp/src/game/server/env_projectedtexture.cpp
+++ mp/src/game/server/env_projectedtexture.cpp
@@ -239,7 +239,18 @@ void CEnvProjectedTexture::Activate( void )
 
 void CEnvProjectedTexture::InitialThink( void )
 {
-	m_hTargetEntity = gEntList.FindEntityByName( NULL, m_target );
+	if ( m_hTargetEntity == NULL && m_target != NULL_STRING )
+		m_hTargetEntity = gEntList.FindEntityByName( NULL, m_target );
+	if ( m_hTargetEntity == NULL )
+		return;
+ 
+	Vector vecToTarget = (m_hTargetEntity->GetAbsOrigin() - GetAbsOrigin());
+	QAngle vecAngles;
+	VectorAngles( vecToTarget, vecAngles );
+	SetAbsAngles( vecAngles );
+ 
+	SetNextThink( gpGlobals->curtime + 0.1 );
+	//FIXME MORE LATER, seems like something else can go in here for something cool, like vibration settings
 }
 
 int CEnvProjectedTexture::UpdateTransmitState()
