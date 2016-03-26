c9e1d2e0a42d94ed011d215bf9215e3388f311fa
bmk10 <gchipunov@hotmail.com>
2016-03-25 23:02:16 -0700

    how to make things simplier 4 debuggingInterfaces
    the problem wtih source and high load of newbies trying to attempt to code it newbie C++ coders + llots of buggier code to read, - no one know what engine size means vs having to be honoring the spam system of games.

---
+++ mp/src/game/shared/mod_interface/master_mod_interface_control_unit.h
@@ -0,0 +1,19 @@
+#ifdef I'm here die
+
+
+
+calc computer memory vs cpu strength vs player idioticly
+.
+
+
+initialize class soon but after 10 % runtime take some numbers
+
+
+
+only take numbers if needed.
+
+
+send 2 ints per sync call to server thread.
+
+
+make thread hax dll exe launch, hate windows.
